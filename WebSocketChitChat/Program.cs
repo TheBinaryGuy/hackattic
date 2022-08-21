7584bfaf0ba14a1562b42546b08e45923475f7d9
TheBinaryGuy <hello@thebinary.dev>
2022-08-21 22:49:33 +0530

    Finished WebSocketChitChat.

--- WebSocketChitChat/Program.cs
+++ WebSocketChitChat/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Net.Http.Json;
 using System.Net.WebSockets;
@@ -9,7 +10,7 @@ IConfiguration config = new ConfigurationBuilder()
     .AddUserSecrets(typeof(Program).Assembly)
     .Build();
 
-var client = new HttpClient
+using var client = new HttpClient
 {
     BaseAddress = new Uri("https://hackattic.com/")
 };
@@ -22,8 +23,8 @@ if (string.IsNullOrWhiteSpace(accessToken))
 
 var problemSet = await client.GetFromJsonAsync<GetProblemSet>($"challenges/websocket_chit_chat/problem?access_token={accessToken}");
 
-var wsClient = new ClientWebSocket();
-var secret = string.Empty;
+using var wsClient = new ClientWebSocket();
+var secret = "square limit withered water yellow sun";
 var sw = new Stopwatch();
 
 await wsClient.ConnectAsync(new Uri($"wss://hackattic.com/_/ws/{problemSet!.token}"), CancellationToken.None);
@@ -34,13 +35,21 @@ while (wsClient.State == WebSocketState.Open)
     var buffer = new ArraySegment<byte>(new byte[1024]);
     await wsClient.ReceiveAsync(buffer, CancellationToken.None);
     var message = Encoding.UTF8.GetString(buffer);
-    Console.WriteLine(message);
+    Console.WriteLine($"Message: {message}");
     if (message.StartsWith("ping!"))
     {
-        var interval = sw.Elapsed.Milliseconds - (sw.Elapsed.Milliseconds % 100);
-        Console.WriteLine(interval);
-        await wsClient.SendAsync(Encoding.UTF8.GetBytes(interval.ToString()), WebSocketMessageType.Text, false, CancellationToken.None);
+        var timeElapsed = sw.Elapsed.TotalMilliseconds;
         sw.Restart();
+        var interval = GetInterval(timeElapsed);
+        await wsClient.SendAsync(Encoding.UTF8.GetBytes(interval.ToString()), WebSocketMessageType.Text, true, CancellationToken.None);
+        Console.WriteLine($"Time Elapsed: {timeElapsed} | Interval Sent: {interval}");
+    }
+
+    if (message.StartsWith("congratulations!"))
+    {
+        var from = message.IndexOf("\"");
+        var to = message.LastIndexOf("\"");
+        secret = message[(from + 1)..to];
     }
 }
 
@@ -51,14 +60,24 @@ if (string.IsNullOrWhiteSpace(secret))
     return;
 }
 
+Console.WriteLine($"Solution: {secret}");
+
 var solutionResponse = await client.PostAsJsonAsync<SubmitSolution>(
     $"challenges/websocket_chit_chat/solve?access_token={accessToken}", // add "&playground=1" to replay this challenge
     new(secret)
 );
 
 var responseBody = await solutionResponse.Content.ReadAsStringAsync();
-Console.WriteLine(responseBody);
-Console.ReadLine();
+Console.WriteLine($"Response: {responseBody}");
+
+static int GetInterval(double elapsedMs)
+{
+    var possibleIntervals = new int[] { 700, 1500, 2000, 2500, 3000 };
+    var values = new ConcurrentBag<(int interval, double value)>();
+    Parallel.ForEach(possibleIntervals, interval => values.Add((interval, elapsedMs / interval)));
+
+    return values.OrderBy(x => Math.Abs(x.value - 1)).First().interval;
+}
 
 internal record GetProblemSet(string token);
 internal record SubmitSolution(string secret);
